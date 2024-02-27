597abe82a6d056b7fcd7d349392885d3a61ece30
Renato Pouseiro <paulo.pouseiro@ua.pt>
2024-02-27 09:55:04 +0000

    Define dailytemp.c struct

--- dailytemp.c
+++ dailytemp.c
@@ -1,1 +1,22 @@
+#include
 
+void dtInit(int *vect)
+{
+
+}
+void dtAdd(int *vect)
+{
+
+}
+void dtStat(int *vect, int max, int min, int avg)
+{
+    
+}
+int main()
+{
+    while(1)
+    {
+
+    }
+    return 0;
+}
